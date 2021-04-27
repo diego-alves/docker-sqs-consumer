f86b3ee41665174e7d0e039f52a3995d8683fc38
Diego Alves <dalves@pagseguro.com>
2021-04-26 21:12:09 -0300

    ok

--- docker_sqs_consumer/main.py
+++ docker_sqs_consumer/main.py
@@ -7,10 +7,15 @@ sqs = resource('sqs')
 def main():
   print("Start Consuming Queue")
   queue = sqs.get_queue_by_name(QueueName="MyQueue")
-  for message in queue.receive_messages():
-    print(loads(message.body))
-    message.delete()
+  while True:
+    for message in queue.receive_messages():
+      try:
+        print(loads(message.body))
+      except Exception as e:
+        print(e)
+      finally:
+        message.delete()
 
 if __name__ == "__main__":
-  while True:
-    main()
+  main()
+
